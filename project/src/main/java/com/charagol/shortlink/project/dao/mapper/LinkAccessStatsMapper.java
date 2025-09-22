cb74ea44b8c58821c8213e51da9219f0aba6f05e
Charagol <15573218641@163.com>
2025-09-22 16:10:21 +0800

    fix: 修复数据库中条目不累加

--- project/src/main/java/com/charagol/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
+++ project/src/main/java/com/charagol/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
@@ -18,26 +18,29 @@
 package com.charagol.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
 import com.charagol.shortlink.project.dao.entity.LinkAccessStatsDO;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
 
 /**
  * 短链接基础访问监控持久层
- * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
  */
+@Mapper
 public interface LinkAccessStatsMapper extends BaseMapper<LinkAccessStatsDO> {
 
     /**
      * 记录基础访问监控数据
      */
+    // 如果设置了gid参与唯一索引，而这里插入时不传gid，每次跳转会生成新的条目，而不是累加。
+    // 原因在于不传则gid为null，而 null 不等于任何值，甚至 “ null 也不等于 null ”
+    // 因此每次添加时由于gid的不同，会生成新条目。
+
+    // 唯一索引：idx_unique_access_stats (full_short_url, gid, weekday, hour)
+    // 只有当这四个字段的值完全一样时，数据库才会认为是重复记录 ———— 如果重复，针对 ON DUPLICATE KEY，执行 UPDATE
     @Insert("INSERT INTO " +
-            "t_link_access_stats (full_short_url, date, pv, uv, uip, hour, weekday, create_time, update_time, del_flag) " +
-            "VALUES( #{linkAccessStats.fullShortUrl}, #{linkAccessStats.date}, #{linkAccessStats.pv}, #{linkAccessStats.uv}, #{linkAccessStats.uip}, #{linkAccessStats.hour}, #{linkAccessStats.weekday}, NOW(), NOW(), 0) " +
+            "t_link_access_stats (full_short_url,gid, date, pv, uv, uip, hour, weekday, create_time, update_time, del_flag) " +
+            "VALUES( #{linkAccessStats.fullShortUrl},#{linkAccessStats.gid}, #{linkAccessStats.date}, #{linkAccessStats.pv}, #{linkAccessStats.uv}, #{linkAccessStats.uip}, #{linkAccessStats.hour}, #{linkAccessStats.weekday}, NOW(), NOW(), 0) " +
             "ON DUPLICATE KEY UPDATE pv = pv +  #{linkAccessStats.pv}, uv = uv + #{linkAccessStats.uv}, uip = uip + #{linkAccessStats.uip};")
     void shortLinkStats(@Param("linkAccessStats") LinkAccessStatsDO linkAccessStatsDO);
 }
