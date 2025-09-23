65a4351a8165d8714e72a7e465ba3753bea4d267
Charagol <15573218641@163.com>
2025-09-23 22:42:36 +0800

    feature: 开发短链接后管项目调用监控接口

--- admin/src/main/java/com/charagol/shortlink/admin/remote/dto/resp/ShortLinkStatsAccessDailyRespDTO.java
+++ admin/src/main/java/com/charagol/shortlink/admin/remote/dto/resp/ShortLinkStatsAccessDailyRespDTO.java
@@ -1,26 +1,22 @@
-package com.charagol.shortlink.project.dto.resp;
+package com.charagol.shortlink.admin.remote.dto.resp;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 /**
  * 短链接基础访问监控响应参数
  */
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class ShortLinkStatsAccessDailyRespDTO {
 
     /**
      * 日期
      */
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private String date;
+    private Date date;
 
     /**
      * 访问量
