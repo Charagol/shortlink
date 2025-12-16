a28f2b5ec2f8a777448f4b4d3f4cf8c1123bb606
Charagol <15573218641@163.com>
2025-12-16 15:22:36 +0800

    fix: 修复聚合服务远程部署服务器失败问题

--- admin/src/main/java/com/charagol/shortlink/admin/ShortLinkAdminApplication.java
+++ admin/src/main/java/com/charagol/shortlink/admin/ShortLinkAdminApplication.java
@@ -1,7 +1,6 @@
 
 package com.charagol.shortlink.admin;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,7 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients("com.charagol.shortlink.admin.remote")
-@MapperScan("com.charagol.shortlink.admin.dao.mapper")
+//@MapperScan("com.charagol.shortlink.admin.dao.mapper")
 public class ShortLinkAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkAdminApplication.class,args);
--- project/src/main/java/com/charagol/shortlink/project/ShortLinkApplication.java
+++ project/src/main/java/com/charagol/shortlink/project/ShortLinkApplication.java
@@ -1,6 +1,5 @@
 package com.charagol.shortlink.project;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@MapperScan("com.charagol.shortlink.project.dao")
+//@MapperScan("com.charagol.shortlink.project.dao")
 public class ShortLinkApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkApplication.class, args);
